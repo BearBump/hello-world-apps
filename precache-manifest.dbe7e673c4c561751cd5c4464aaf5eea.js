a54047e211eb3e653103c5cb5246a1b5723dc61b
BearBump <legendbearbump@gmail.com>
2020-03-21 18:10:24 +0500

    Updates

---
+++ precache-manifest.dbe7e673c4c561751cd5c4464aaf5eea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5de3f91b8727ff430a948b883eda3b83",
+    "url": "/hello-world-apps/index.html"
+  },
+  {
+    "revision": "40dfe62b30bebbbecaea",
+    "url": "/hello-world-apps/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "07bda77d451f60d2bfe3",
+    "url": "/hello-world-apps/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "40dfe62b30bebbbecaea",
+    "url": "/hello-world-apps/static/js/2.48601860.chunk.js"
+  },
+  {
+    "revision": "abf0b813b0144a2f8f7c56edfbc053fa",
+    "url": "/hello-world-apps/static/js/2.48601860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07bda77d451f60d2bfe3",
+    "url": "/hello-world-apps/static/js/main.a27b7b9d.chunk.js"
+  },
+  {
+    "revision": "cee8b5d4062a769bf485",
+    "url": "/hello-world-apps/static/js/runtime-main.957fdde6.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/hello-world-apps/static/media/persik.4e1ec840.png"
+  }
+]);
