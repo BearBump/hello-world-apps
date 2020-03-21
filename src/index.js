ff6c470737a8d773a624d26ee524440d102cf1e2
BearBump <legendbearbump@gmail.com>
2020-03-21 18:23:44 +0500

    2

--- src/index.js
+++ src/index.js
@@ -11,6 +11,20 @@ import * as erudaDom from 'eruda-dom';
 // Init VK  Mini App
 bridge.send('VKWebAppInit');
 
+bridge
+	.sendPromise('VKWebAppGetCommunityAuthToken', {
+		'app_id': 7367831,
+		'group_id': 167045707,
+		"scope": "app_widget"
+	})
+	.then(data => {
+		// Success
+		console.log(data);
+	})
+	.catch(error => {
+		// Error
+	});
+
 ReactDOM.render(<App />, document.getElementById('root'));
 if (process.env.NODE_ENV === 'development') {
 	eruda.init();
